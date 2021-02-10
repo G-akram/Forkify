bef224d63e0f64c4ecc24519a72eb69e318bd838
Ghomari Akram <gaa.akram.aag@gmail.com>
2021-02-10 13:13:56 +0100

    deploy app

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '07fd1276-3ec2-44a3-bb52-ffcceca811d8';
+export const MODAL_CLOSE_SEC = 2.5;
